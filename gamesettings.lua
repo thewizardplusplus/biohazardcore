e92cf61103efbe38ea5f6b58d29697ec707b375c
thewizardplusplus <thewizardplusplus@yandex.ru>
2020-08-29 23:00:00 +0300

    Add the GameSettings class

---
+++ gamesettings.lua
@@ -0,0 +1,44 @@
+---
+-- @classmod GameSettings
+
+local middleclass = require("middleclass")
+local types = require("lualife.types")
+local Stringifiable = require("lualife.models.stringifiable")
+local FieldSettings = require("biohazardcore.fieldsettings")
+
+local GameSettings = middleclass("GameSettings")
+GameSettings:include(Stringifiable)
+
+---
+-- @table instance
+-- @tfield FieldSettings field
+-- @tfield FieldSettings field_part
+
+---
+-- @function new
+-- @tparam FieldSettings field
+-- @tparam FieldSettings field_part
+-- @treturn GameSettings
+function GameSettings:initialize(field, field_part)
+  assert(types.is_instance(field, FieldSettings))
+  assert(types.is_instance(field_part, FieldSettings))
+
+  self.field = field
+  self.field_part = field_part
+end
+
+---
+-- @treturn tab table with instance fields
+function GameSettings:__data()
+  return {
+    field = self.field:__data(),
+    field_part = self.field_part:__data(),
+  }
+end
+
+---
+-- @function __tostring
+-- @treturn string stringified table with instance fields
+-- @see lualife.models.Stringifiable
+
+return GameSettings
