6d056a71694cda07a4dc704183c0eea0fd212ac0
thewizardplusplus <thewizardplusplus@yandex.ru>
2020-08-30 23:00:00 +0300

    Remove stringification from the Game class

--- game.lua
+++ game.lua
@@ -3,14 +3,11 @@
 
 local middleclass = require("middleclass")
 local types = require("lualife.types")
-local Stringifiable = require("lualife.models.stringifiable")
-local Size = require("lualife.models.size")
 local Field = require("lualife.models.field")
 local PlacedField = require("lualife.models.placedfield")
 local GameSettings = require("biohazardcore.models.gamesettings")
 
 local Game = middleclass("Game")
-Game:include(Stringifiable)
 
 ---
 -- @table instance
@@ -29,20 +26,5 @@ function Game:initialize(settings)
   self._field = Field:new(Size:new(0, 0))
   self._field_part = PlacedField:new(Size:new(0, 0))
 end
-
----
--- @treturn tab table with instance fields
-function Game:__data()
-  return {
-    settings = self._settings:__data(),
-    field = self._field:__data(),
-    field_part = self._field_part:__data(),
-  }
-end
-
----
--- @function __tostring
--- @treturn string stringified table with instance fields
--- @see lualife.models.Stringifiable
 
 return Game
