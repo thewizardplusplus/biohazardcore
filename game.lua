e4b2e050faae1b73a997f03fcf960060660bb740
thewizardplusplus <thewizardplusplus@yandex.ru>
2020-08-30 23:00:00 +0300

    Implement the Game:initialize() method

--- game.lua
+++ game.lua
@@ -6,6 +6,7 @@ local types = require("lualife.types")
 local Field = require("lualife.models.field")
 local PlacedField = require("lualife.models.placedfield")
 local GameSettings = require("biohazardcore.models.gamesettings")
+local random = require("lualife.random")
 
 local Game = middleclass("Game")
 
@@ -23,8 +24,28 @@ function Game:initialize(settings)
   assert(types.is_instance(settings, GameSettings))
 
   self._settings = settings
-  self._field = Field:new(Size:new(0, 0))
-  self._field_part = PlacedField:new(Size:new(0, 0))
+
+  local field_part_sample = PlacedField:new(
+    settings.field.size,
+    settings.field.initial_offset
+  )
+  self._field = random.generate_with_limits(
+    field_sample,
+    settings.field.filling,
+    settings.field.minimal_count,
+    settings.field.maximal_count
+  )
+
+  local field_part_sample = PlacedField:new(
+    settings.field.size,
+    settings.field.initial_offset
+  )
+  self._field_part = random.generate_with_limits(
+    field_part_sample,
+    settings.field_part.filling,
+    settings.field_part.minimal_count,
+    settings.field_part.maximal_count
+  )
 end
 
 return Game
