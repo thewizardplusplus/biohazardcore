c4b638c7273f96bbd1a72440369a2950ad840dd1
thewizardplusplus <thewizardplusplus@yandex.ru>
2020-08-31 23:00:00 +0300

    Use the factory module

--- game.lua
+++ game.lua
@@ -4,13 +4,11 @@
 local middleclass = require("middleclass")
 local types = require("lualife.types")
 local Point = require("lualife.models.point")
-local Field = require("lualife.models.field")
-local PlacedField = require("lualife.models.placedfield")
 local GameSettings = require("biohazardcore.models.gamesettings")
-local random = require("lualife.random")
 local sets = require("lualife.sets")
 local matrix = require("lualife.matrix")
 local life = require("lualife.life")
+local factory = require("biohazardcore.factory")
 
 local Game = middleclass("Game")
 
@@ -28,28 +26,8 @@ function Game:initialize(settings)
   assert(types.is_instance(settings, GameSettings))
 
   self._settings = settings
-
-  local field_sample = PlacedField:new(
-    settings.field.size,
-    settings.field.initial_offset
-  )
-  self._field = random.generate_with_limits(
-    field_sample,
-    settings.field.filling,
-    settings.field.minimal_count,
-    settings.field.maximal_count
-  )
-
-  local field_part_sample = PlacedField:new(
-    settings.field_part.size,
-    settings.field_part.initial_offset
-  )
-  self._field_part = random.generate_with_limits(
-    field_part_sample,
-    settings.field_part.filling,
-    settings.field_part.minimal_count,
-    settings.field_part.maximal_count
-  )
+  self._field = factory.create_field(settings.field)
+  self._field_part = factory.create_field(settings.field_part)
 end
 
 ---
@@ -70,25 +48,15 @@ end
 
 function Game:union()
   local intersected_field_part = sets.intersection(self._field, self._field_part)
-  local has_intersection = intersected_field_part:count() ~= 0
-  if has_intersection then
+  if intersected_field_part:count() ~= 0 then
     return
   end
 
   local field_next = sets.union(self._field, self._field_part)
   field_next = life.populate(field_next)
-  self._field = field_next
 
-  local field_part_sample = PlacedField:new(
-    self._settings.field_part.size,
-    self._settings.field_part.initial_offset
-  )
-  self._field_part = random.generate_with_limits(
-    field_part_sample,
-    self._settings.field_part.filling,
-    self._settings.field_part.minimal_count,
-    self._settings.field_part.maximal_count
-  )
+  self._field = field_next
+  self._field_part = factory.create_field(settings.field_part)
 end
 
 return Game
