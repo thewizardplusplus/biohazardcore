93c0f38fa9ec8a2ee79c489c75d25e853034abc7
thewizardplusplus <thewizardplusplus@yandex.ru>
2025-01-18 08:22:07 +0500

    Fix the tests of the `factory.create_field()` function

--- factory_test.lua
+++ factory_test.lua
@@ -19,26 +19,26 @@ function TestFactory.test_neighbors()
   local wanted_cells
   if _VERSION == "Lua 5.4" then
     wanted_cells = {
-      ["{x = 1,y = 2}"] = true,
-      ["{x = 3,y = 5}"] = true,
-      ["{x = 4,y = 6}"] = true,
+      ["{__name = \"Point\",x = 1,y = 2}"] = true,
+      ["{__name = \"Point\",x = 3,y = 5}"] = true,
+      ["{__name = \"Point\",x = 4,y = 6}"] = true,
     }
   elseif _VERSION == "Lua 5.3" or _VERSION == "Lua 5.2" then
     wanted_cells = {
-      ["{x = 0,y = 8}"] = true,
-      ["{x = 1,y = 9}"] = true,
+      ["{__name = \"Point\",x = 0,y = 8}"] = true,
+      ["{__name = \"Point\",x = 1,y = 9}"] = true,
     }
   elseif _VERSION == "Lua 5.1" then
     if type(jit) == "table" then -- check for LuaJIT
       wanted_cells = {
-        ["{x = 0,y = 10}"] = true,
-        ["{x = 0,y = 1}"] = true,
-        ["{x = 0,y = 3}"] = true,
+        ["{__name = \"Point\",x = 0,y = 10}"] = true,
+        ["{__name = \"Point\",x = 0,y = 1}"] = true,
+        ["{__name = \"Point\",x = 0,y = 3}"] = true,
       }
     else
       wanted_cells = {
-        ["{x = 1,y = 8}"] = true,
-        ["{x = 2,y = 9}"] = true,
+        ["{__name = \"Point\",x = 1,y = 8}"] = true,
+        ["{__name = \"Point\",x = 2,y = 9}"] = true,
       }
     end
   end
